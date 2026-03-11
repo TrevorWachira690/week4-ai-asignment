85060768ac1e436c4309013b3cbdb1aa3547e814
TrevorWachira <greatgaming656@gmail.com>
2026-03-11 07:09:48 +0300

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,21 @@
-
+```python
+print("Hello AI Students")
+Name= "Trevor"
+Age= 20
+City= "Nairobi"
+print("Name:",Name)
+#Name: Trevor
+print("Age:",Age)
+#Age: 20
+print("City:",City)
+#City: Nairobi
+languages= ["Python","Java","C++","JavaScript","Go"]
+print(languages[0])
+student= {
+  "Name":"Trevor",
+  "Age":20,
+  "Course":"AI"
+}
+print(student["Name"])
+#Output- Trevor
+```
