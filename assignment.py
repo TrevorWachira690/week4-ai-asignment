f6a48d6a5079bb4fe5b13646e98583450fd502b5
TrevorWachira <greatgaming656@gmail.com>
2026-03-11 07:24:22 +0300

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,5 +1,8 @@
 ```python
+# QUESTION 4
 print("Hello AI Students")
+
+# QUESTION 5
 Name= "Trevor"
 Age= 20
 City= "Nairobi"
@@ -9,8 +12,13 @@ print("Age:",Age)
 #Age: 20
 print("City:",City)
 #City: Nairobi
+
+# QUESTION 6
 languages= ["Python","Java","C++","JavaScript","Go"]
 print(languages[0])
+#Output- python
+
+# QUESTION 7
 student= {
   "Name":"Trevor",
   "Age":20,
@@ -19,3 +27,5 @@ student= {
 print(student["Name"])
 #Output- Trevor
 ```
+# BONUS QUESTION
+# A list is an ordered collection of elements that support different data types whereas a dictionary is a coll3ction of key-value pairs enclosed in curly brackets.
